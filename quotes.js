ecdac18b879f0efb8ea7b25e4c58900e52f834ef
Kilian <kilian_dr@aol.de>
2024-03-02 12:54:47 +0100

    adding starting project

---
+++ quotes.js
@@ -0,0 +1,46 @@
+const localQuotes = [
+    {
+        text: 'A fit body, a calm mind, a house full of love. These things cannot be bought - they must be earned.',
+        author: 'Naval Ravikant',
+    },
+    {
+        text: 'Peace is something that starts with me.',
+        author: 'Kid Cudi',
+    },
+    {
+        text: 'Most things I worry about never happen anyway.',
+        author: 'Tom Petty',
+    },
+    {
+        text: 'What you see isn’t all there is.',
+        author: 'Unknown',
+    },
+    {
+        text: 'If you have the opportunity to play this game called life, you have to appreciate every moment. A lot of people don’t appreciate their moment until it’s passed.',
+        author: 'Kanye West',
+    },
+    {
+        text: 'Your self respect is what matters. You cannot have a good life or have other people respect you for that matter if you don’t respect yourself. ',
+        author: 'XXXTentacion',
+    },
+    {
+        text: 'Anyone who isn’t embarrassed of who they were last year probably isn’t learning enough.',
+        author: 'Alain de Botton',
+    },
+    {
+        text: 'Don’t miss the point. You are supposed to sing or dance while the music is being played.',
+        author: 'Alan Watts',
+    },
+    {
+        text: 'It’s not because things are difficult that we dare not venture. It’s because we dare not venture that they are difficult.',
+        author: 'Seneca',
+    },
+    {
+        text: 'It is not what happens to you, but how you react to it that matters.',
+        author: 'Epictetus',
+    },
+    {
+        text: 'The energy of the mind is the essence of life.',
+        author: 'Aristotle',
+    },
+]
